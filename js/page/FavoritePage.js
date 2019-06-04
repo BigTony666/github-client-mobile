cda4a778c4be19f9901dfa503c886b45e37864be
BigTony <kejiezhang666@gmail.com>
2019-06-04 17:18:18 -0400

    feat: modify top tabs

--- js/page/FavoritePage.js
+++ js/page/FavoritePage.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 type Props = {};
 export default class FavoritePage extends Component<Props> {
   render() {
+    const {navigation} = this.props;
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}> FavoritePage </Text>
+        <Button
+          title="Change Theme Color"
+          onPress={() => {
+            navigation.setParams({
+              theme: {
+                tintColor: 'blue',
+                updateTime: new Date().getTime(),
+              }
+            })
+          }}
+        />
       </View>
     );
   }
